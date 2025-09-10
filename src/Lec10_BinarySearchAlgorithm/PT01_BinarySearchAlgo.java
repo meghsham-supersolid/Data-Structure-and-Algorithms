8fc1d0d114e4cb63f8bf6bb6cedc1fc392d41868
Meghsham <meghshamvinayakkapure@gmail.com>
2025-09-10 11:23:39 +0530

    Learned about Binary Search

---
+++ src/Lec10_BinarySearchAlgorithm/PT01_BinarySearchAlgo.java
@@ -0,0 +1,27 @@
+package Lec10_BinarySearchAlgorithm;
+
+public class PT01_BinarySearchAlgo {
+    public static void main(String[] args) {
+        int[] arr = new int[]{17, 22, 40, 55, 63, 68, 89, 89, 97};
+        int searchValue = 34;
+        System.out.println(binarySearchAlgo(arr, searchValue));
+    }
+
+    public static int binarySearchAlgo(int[] arr, int searchValue) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+//            int mid = (start + end) / 2;
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == searchValue) {
+                return mid;
+            } else if (arr[mid] < searchValue) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/_Extras/RandomArrayGenerator.java
@@ -0,0 +1,36 @@
+package _Extras;
+
+import java.util.Arrays;
+
+public class RandomArrayGenerator {
+
+    public static void main(String[] args) {
+        int[] arr = intArrayGenerator(10);
+        System.out.println(Arrays.toString(intArrayGeneratorInAscendingOrder(arr)));
+        System.out.println(Arrays.toString(intArrayGeneratorInDescendingOrder(arr)));
+    }
+
+    public static int[] intArrayGenerator(int size) {
+        int arr[] = new int[size];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 1000 + 1);
+        }
+        return arr;
+    }
+
+    public static int[] intArrayGeneratorInAscendingOrder(int[] arr) {
+        Arrays.sort(arr);
+        return arr;
+    }
+
+    public static int[] intArrayGeneratorInDescendingOrder(int[] arr) {
+        int[] newArr = new int[arr.length];
+        for (int i = 0; i < newArr.length; i++) {
+            newArr[i] = arr[arr.length - 1 - i];
+        }
+
+        return newArr;
+    }
+
+}
